924e31535b6e10772ee737077c1b643285a27397
rhsagar145 <>
2022-05-27 17:53:26 -0400

    dropdown

--- js/script.js
+++ js/script.js
@@ -1,1 +1,12 @@
-console.log('JavaScript is linked!');
+function myFunction() {
+	document.getElementById('myDropdown').classList.toggle('show');
+}
+
+window.onclick = function (e) {
+	if (!e.target.matches('.dropbtn')) {
+		var myDropdown = document.getElementById('myDropdown');
+		if (myDropdown.classList.contains('show')) {
+			myDropdown.classList.remove('show');
+		}
+	}
+};
