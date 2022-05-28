b93fe0df29f46f4f39dc360ebaa4d11c4077319d
rhsagar145 <>
2022-05-28 14:54:45 -0400

    modal1

--- js/script.js
+++ js/script.js
@@ -11,3 +11,26 @@ window.onclick = function (e) {
 	}
 };
 
+const modal = document.getElementById('simpleModal');
+const modalBtn = document.getElementById('modalBtn');
+const closeBtn = document.getElementsByClassName('closeBtn')[0];
+
+modalBtn.addEventListener('click', openModal);
+
+closeBtn.addEventListener('click', closeModal);
+
+window.addEventListener('click', clickOutside);
+
+function openModal() {
+	modal.style.display = 'block';
+}
+
+function closeModal() {
+	modal.style.display = 'none';
+}
+
+function clickOutside(e) {
+	if (e.target == modal) {
+		modal.style.display = 'none';
+	}
+}
